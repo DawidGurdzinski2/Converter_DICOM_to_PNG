1e7dd79399c52ab55ba1189e63459327b9ab986d
DawidGurdzinski2 <gurdzinskidawid@gamil.com>
2022-11-13 18:39:23 +0100

    Conversion Dicom to PNG

--- Converter/MainWindow.py
+++ Converter/MainWindow.py
@@ -2,7 +2,9 @@ import tkinter as tk
 from ConversionFrame import *
 from MainButton import *
 from tkinter import filedialog as fd
-
+import pydicom 
+import numpy as np 
+from PIL import Image
 class MainWindow:
 
 
@@ -10,6 +12,7 @@ class MainWindow:
         self.window=window
         self.initialdir="/home/dawid/Desktop/Programing and other" #linux
         #self.initModules()
+        self.filetypes=(  ("Dicom Files", "*.dcm*"),("All Files", "*.*"))
         self.frame = tk.Frame(self.window,bg="white",bd=5)
         tk.Grid.rowconfigure(self.window,0,weight=1)
         tk.Grid.columnconfigure(self.window,0,weight=1)
@@ -32,10 +35,13 @@ class MainWindow:
 
     def addFiles(self):
         try:
-            file_path=fd.askopenfile(initialdir=self.initialdir).name
+            file_path=fd.askopenfile(initialdir=self.initialdir,filetypes=self.filetypes).name
+            print(file_path)
             if file_path in self.cframe.getFiles():
                print("jest juz")
-            else:
+               
+            elif(len(str(file_path))>2):
+                #print(len(str(file_path)))
                 self.cframe.addDicomFile(file_path)
                 self.window.update_idletasks()
         except:
@@ -60,9 +66,17 @@ class MainWindow:
             
         
     def converFiles(self):
-        print(self.cframe.getSelectedFiles())  
-        print(self.cframe.getFiles())  
-    
+        self.readDicomFile()
 
+    def readDicomFile(self,path):
+        dicom_images=[]
+        for path in self.cframe.getSelectedFiles():
+            dicom=pydicom.dcmread(path)
+            dicom=dicom.pixel_array.astype(float)
+            rescaledImage=((np.maximum(dicom,0)/dicom.max())*255)
+            finalImage=np.uint8(rescaledImage)
+            finalImage=Image.fromarray(finalImage)
+            finalImage.show()
+            finalImage.save()
 
     
