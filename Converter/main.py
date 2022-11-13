175be5ffe3a94f98709579b2cf160ce250a31054
DawidGurdzinski2 <gurdzinskidawid@gamil.com>
2022-11-13 03:10:28 +0100

    name main

--- Converter/main.py
+++ Converter/main.py
@@ -3,21 +3,29 @@ from tkinter import messagebox
 import tkinter as tk
 from MainWindow import *
 
-window =tk.Tk() 
-windowHeight=500
-windowWidth=800
-#window.resizable(False,False)
-window.geometry(str(windowWidth)+"x"+str(windowHeight))
-window.title("Main Window")
-window.config(background="white")
-frame=MainWindow(window)
 
 
 
 
-def on_closing():
-    if messagebox.askokcancel("Quit", "Do you want to quit?"):
-        window.destroy()
-window.protocol("WM_DELETE_WINDOW", on_closing)
-window.mainloop() 
+def main():
+    window =tk.Tk() 
+    windowHeight=500
+    windowWidth=800
+    #window.resizable(False,False)
+    window.geometry(str(windowWidth)+"x"+str(windowHeight))
+    window.title("Main Window")
+    window.config(background="white")
+    frame=MainWindow(window)
 
+
+
+
+    def on_closing():
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            window.destroy()
+    window.protocol("WM_DELETE_WINDOW", on_closing)
+    window.mainloop() 
+
+
+if __name__== '__main__':
+    main()
